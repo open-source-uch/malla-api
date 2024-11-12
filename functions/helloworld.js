d470f2e7b8918912569f9a458e654781ea1c8b7b
Tom Villegas <tvillega@dcc.uchile.cl>
2024-11-11 21:39:28 -0300

    feat: return a multine string

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,11 @@
 export function onRequest(context) {
-  return new Response("Hello, world!")
+
+const = greet `{
+    id: 2,
+    title: "Updating my blog",
+    text: "It's my second blog post! I'm still writing and publishing using Cloudflare Workers + Pages :)",
+    published_at: new Date("2020-10-26"),
+  }`
+
+  return new Response(greet)
 }
