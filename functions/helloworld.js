45bd874f4fa73ae36d33108156c46c81c0172162
Tom Villegas <tvillega@dcc.uchile.cl>
2024-11-11 22:05:20 -0300

    fix: ft chat dcc

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,11 +1,11 @@
 export function onRequest(context) {
 
-let greet: string `{
+const greet = `{
     id: 2,
     title: "Updating my blog",
-    text: "It's my second blog post! I'm still writing and publishing using Cloudflare Workers + Pages :)",
+    text: "It's my second blog post!",
     published_at: new Date("2020-10-26"),
-  }`
+  }`;
 
   return new Response(greet)
 }
